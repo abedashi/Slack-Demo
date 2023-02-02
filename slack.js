ce0c58b5a6d3e55ad422fbf1b75f51fa00548d0e
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-02-03 00:08:17 +0200

    server socket connection from main namespace and one of others

--- slack.js
+++ slack.js
@@ -2,6 +2,28 @@ const express = require('express');
 const app = express();
 const socketio = require('socket.io');
 
+let namespaces = require('./data/namespaces');
+
 app.use(express.static(__dirname + '/public'));
 const server = app.listen(3001);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    // Build an array to send back with the img and endpoint for each namespace
+    let nsData = namespaces.map((ns) => {
+        return {
+            img: ns.img,
+            endpoint: ns.endpoint
+        }
+    });
+
+    // Send nsData to client
+    socket.emit('nsList', nsData);
+});
+
+namespaces.forEach((namespace) => {
+    io.of(namespace.endpoint).on('connection', (nsSocket) => {
+        console.log(`${nsSocket.id} has join ${namespace.endpoint}`);
+        nsSocket.emit('nsRoomLoad', namespaces[0].rooms);
+    });
+})

ba1c9f76217675ef89d2d75b846ebfd66580a837
Abed al salam Ashi <abed_ashi@hotmail.com>
2023-02-04 01:18:21 +0200

    add and subtract member on joining and leaving room

--- slack.js
+++ slack.js
@@ -9,6 +9,7 @@ const server = app.listen(3001);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
+    // console.log(socket.handshake);
     // Build an array to send back with the img and endpoint for each namespace
     let nsData = namespaces.map((ns) => {
         return {
@@ -21,39 +22,47 @@ io.on('connection', (socket) => {
     socket.emit('nsList', nsData);
 });
 
+const updateMember = async (namespace, roomToJoin) => {
+    const clients = await io.of(namespace.endpoint).in(roomToJoin).allSockets();
+    io.of(namespace.endpoint).in(roomToJoin).emit('updateMembers', clients.size);
+};
+
 namespaces.forEach((namespace) => {
     io.of(namespace.endpoint).on('connection', (nsSocket) => {
-        console.log(`${nsSocket.id} has join ${namespace.endpoint}`);
-        nsSocket.emit('nsRoomLoad', namespaces[0].rooms);
-        nsSocket.on('joinRoom', async (roomToJoin) => {
+        const username = nsSocket.handshake.query.username
+        // console.log(`${nsSocket.id} has join ${namespace.endpoint}`);
+        nsSocket.emit('nsRoomLoad', namespace.rooms);
+        nsSocket.on('joinRoom', (roomToJoin) => {
+            // leave previous room
+            const roomToLeave = Array.from(nsSocket.rooms)[1];
+            nsSocket.leave(roomToLeave);
+            updateMember(namespace, roomToLeave);
             // Deal with history... Once we have it.
             nsSocket.join(roomToJoin);
-            const clients = await nsSocket.in(roomToJoin).allSockets();
-            // numberOfUsersCallback(clients.size);
-            io.of('/wiki').in(roomToJoin).emit('updateMembers', clients.size);
 
-            const nsRoom = namespaces[0].rooms.find((room) => {
+            const nsRoom = namespace.rooms.find((room) => {
                 return room.roomTitle === roomToJoin;
             });
             nsSocket.emit('history', nsRoom.history);
+            updateMember(namespace, roomToJoin);
         });
 
         nsSocket.on('newMessageToServer', (msg) => {
             const fullMsg = {
                 text: msg.text,
                 time: Date.now(),
-                username: 'ashie',
+                username: username,
                 avatar: 'https://via.placeholder.com/30'
             };
             // Send this message to all the sockets that are in the room this SOCKET is in.
             // Room should always on index [1] Set(2) { '12SP7zQIAw4OYQmwAAAH', 'New Articles' }
             const roomTitle = Array.from(nsSocket.rooms)[1];
-            const nsRoom = namespaces[0].rooms.find((room) => {
+            const nsRoom = namespace.rooms.find((room) => {
                 return room.roomTitle === roomTitle;
             });
-            console.log(nsRoom);
+            // console.log(nsRoom);
             nsRoom.addMessage(fullMsg);
-            io.of('/wiki').to(roomTitle).emit('messageToClients', fullMsg);
+            io.of(namespace.endpoint).to(roomTitle).emit('messageToClients', fullMsg);
         });
     });
 })
